a20791afcc86590a9ea691e1df8cd82329df9592
Ahmad Saadeddin <AS@ahm4d.com>
2020-12-13 19:10:55 +0200

    components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import Todos from "./Todos";
+import Goals from "./Goals";
+import { connect } from "react-redux";
+import { handleFetch } from "../actions/shared";
 
-export default function App () {
-  return (
-    <div>
-      Goals - Todos
-    </div>
-  )
+class App extends React.Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(handleFetch());
+  }
+  render() {
+    const { loading } = this.props;
+    return (
+      <div>
+        {loading ? (
+          <h3>Loading</h3>
+        ) : (
+          <div>
+            <Todos />
+            <Goals />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
+
+export default connect((state) => ({
+  loading: state.loading,
+}))(App);

3b1c22927060c752a34f9ea5ded9fff1d0b692d9
ahmadsaadeddins <68161008+ahmadsaadeddins@users.noreply.github.com>
2020-12-14 08:22:40 +0200

    update 1- actions

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
+const add_todo = {
+  type: ADD_TODO,
+  todo: {
+    id: 0,
+    name: "Learn Redux",
+    complete: false,
+  },
+};
+
+const removeTodo = {
+  type: REMOVE_TODO,
+  id: 0,
+};
+
+const toggleTodo = {
+  type: TOGGLE_TODO,
+  id: 0,
+};
+
+const add_goal = {
+  type: ADD_GOAL,
+  goal: {
+    id: 0,
+    name: "Run a marthon",
+  },
+};
+
+const removeGoal = {
+  type: REMOVE_GOAL,
+  id: 0,
+};
+
 function createStore() {
   // The store should have four parts
   // 1. the state
   // 2. Get the state
   // 3. Listen to changes on the state.
   // 4. Update the state
-  let state;
-  let listeners = [];
 
+  // 1. the state
+  let state;
+
+  // 2. Get the state
   const getState = () => state;
 
+  // 3. Listen to changes on the state.
+  let listeners = [];
   const subscribe = (listener) => {
     listeners.push(listener);
 

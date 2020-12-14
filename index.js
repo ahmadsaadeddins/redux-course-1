9064564d28fc1ec912536b138de825875fc1e5a0
ahmadsaadeddins <68161008+ahmadsaadeddins@users.noreply.github.com>
2020-12-14 10:18:40 +0200

    actions creators

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
+// Library Code
+function createStore(reducer) {
+  // The store should have four parts
+  // 1. the state
+  let state;
+
+  // 2. Get the state
+  const getState = () => state;
+
+  // 3. Listen to changes on the state.
+  let listeners = [];
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    // Unsubscribe it from listeners
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
+  // 4. Update the state
+  const dispatch = (action) => {
+    state = reducer(state, action);
+
+    // Invoke subscribed functions we are saved in listeners
+    listeners.forEach((listener) => listener());
+  };
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  };
+}
+
+// App code
 const ADD_TODO = "ADD_TODO";
 const REMOVE_TODO = "REMOVE_TODO";
 const TOGGLE_TODO = "TOGGLE_TODO";
 const ADD_GOAL = "ADD_GOAL";
 const REMOVE_GOAL = "REMOVE_GOAL";
 
-const add_todo = {
-  type: ADD_TODO,
-  todo: {
-    id: 0,
-    name: "Learn Redux",
-    complete: false,
-  },
-};
-
-const removeTodo = {
-  type: REMOVE_TODO,
-  id: 0,
-};
-
-const toggleTodo = {
-  type: TOGGLE_TODO,
-  id: 1,
-};
-
-const add_goal = {
-  type: ADD_GOAL,
-  goal: {
-    id: 0,
-    name: "Run a marthon",
-  },
-};
-
-const removeGoal = {
-  type: REMOVE_GOAL,
-  id: 0,
-};
-// End of actions
+const addTodoAction = (todo) => ({ type: ADD_TODO, todo });
+const removeTodoAction = (id) => ({ type: REMOVE_TODO, id });
+const toggleTodoAction = (id) => ({ type: TOGGLE_TODO, id });
+const addGoalAction = (goal) => ({ type: ADD_GOAL, goal });
+const removeGoalAction = (id) => ({ type: REMOVE_GOAL, id });
 
 // Reducer
 function todos(state = [], action) {
@@ -74,52 +83,46 @@ function rootReducer(state = {}, action) {
   };
 }
 
-function createStore(reducer) {
-  // The store should have four parts
-  // 1. the state
-  // 2. Get the state
-  // 3. Listen to changes on the state.
-  // 4. Update the state
-
-  // 1. the state
-  let state;
-
-  // 2. Get the state
-  const getState = () => state;
-
-  // 3. Listen to changes on the state.
-  let listeners = [];
-  const subscribe = (listener) => {
-    listeners.push(listener);
-
-    // Unsubscribe it from listeners
-    return () => {
-      listeners = listeners.filter((l) => l !== listener);
-    };
-  };
-
-  // 4. Update the state
-  const dispatch = (action) => {
-    console.log(state);
-
-    state = reducer(state, action);
-    console.log(state);
-
-    // Invoke subscribed functions we are saved in listeners
-    listeners.forEach((listener) => listener());
-  };
-
-  return {
-    getState,
-    subscribe,
-    dispatch,
-  };
-}
-
+// Init store
 const store = createStore(rootReducer);
 const unsubscribe = store.subscribe(() =>
   console.log("the new state is: ", store.getState())
 );
 
-store.dispatch(add_todo);
-store.dispatch(add_goal);
+store.dispatch(
+  addTodoAction({
+    id: 0,
+    name: "Learn Redux",
+    complete: false,
+  })
+);
+
+store.dispatch(
+  addTodoAction({
+    id: 1,
+    name: "Learn React",
+    complete: true,
+  })
+);
+
+store.dispatch(
+  addTodoAction({
+    id: 2,
+    name: "Learn JS",
+    complete: true,
+  })
+);
+
+store.dispatch(
+  addGoalAction({
+    id: 0,
+    name: "Run a marthon",
+  })
+);
+
+store.dispatch(
+  addGoalAction({
+    id: 1,
+    name: "wash the car",
+  })
+);
